e015aaee12d3b0928feaef2455c994c6086b996c
Danya Zverev <i_am_zver@yahoo.com>
2025-05-10 03:28:47 +0500

    login

--- flights/urls.py
+++ flights/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path("flights/<int:flight_id>/", views.flight_detail, name="flight_detail"),
     path("airport_detail/<int:airport_id>/", views.airport_detail, name="airport_detail"),
     path("my_flights/", views.passenger_detail, name="passenger_detail"),
--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.shortcuts import render, get_object_or_404
 from .models import Flight, Airport, Passenger
+from .forms import LoginForm , RegisterForm
+from django.contrib.auth import login, authenticate, logout
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'flights/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.error(request, "Неверный логин или пароль")
+    else:
+        form = LoginForm()
+    return render(request, 'flights/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
 
 def index(request):
     flights = Flight.objects.all()
